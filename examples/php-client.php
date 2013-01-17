4f06459ea6ce5678c34f26ebebc0ed0575ff4941
Igor Partola <igor@igorpartola.com>
2013-01-17 12:23:10 -0500

    A looping example for PHP.

--- examples/php-client.php
+++ examples/php-client.php
@@ -3,7 +3,9 @@
 require_once( dirname(dirname(__file__)) . '/clients/loghog.php');
 
 
-$logger = new Loghog('qq');
+$logger = new Loghog('proga', array('secret' => 'qqq1', 'mode' => Loghog::STREAM));
 
-$logger->debug('And %s and %s and %s', '1', 'b', 'C');
-
+while (1) {
+    $logger->debug('And %s and %s and %s', '1', 'b', 'C');
+    sleep(1);
+}
