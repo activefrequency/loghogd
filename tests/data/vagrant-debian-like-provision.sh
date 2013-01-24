8ff062650248e3ca3e606498785a37d9331aba25
Igor Partola <igor@igorpartola.com>
2013-01-24 15:39:08 -0500

    When doing build tests, use local code, not GitHub.

--- tests/data/vagrant-debian-like-provision.sh
+++ tests/data/vagrant-debian-like-provision.sh
@@ -3,28 +3,16 @@
 set -u
 set -e
 
-GIT_REPO=git://github.com/activefrequency/loghogd.git
-
 DEBIAN_FRONTEND=noninteractive apt-get update -qq > /dev/null
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends -qq git-core build-essential fakeroot devscripts debhelper python-setuptools python-all python-support python-dateutil > /dev/null
 
-mkdir -p ~/.ssh
-echo '|1|sHRXzAmag5MqMsCeKD9BmOhagtY=|+G4gxq9iWiwvSDRrYIS+k/DJ7ko= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==' >> ~/.ssh/known_hosts
-echo '|1|pS1tYfKvNmzGxc1bhj2XEMOdEp4=|99GxsSrQQpuf4QiVKKy6cZ33DQ8= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==' >> ~/.ssh/known_hosts
-
-rm -rf loghogd
-git clone --quiet "$GIT_REPO" loghogd
 cd loghogd
-
-git submodule --quiet init
-git submodule --quiet update
-
 
 debuild -us -uc -I'.git'
 
-dpkg -i ../loghog*_all.deb
+cd ..
 
-cd
+dpkg -i loghogd*_all.deb
 
 /etc/init.d/loghogd status
 /etc/init.d/loghogd stop
